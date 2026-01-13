d6d55846dc13e46617f4bd2ee743279b3c196925
Ryan Xu <RyanXu1979@gmail.com>
2026-01-13 19:00:10 +0800

    🐞 fix: Remove hello panel

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,7 @@
 import { commands, window, ExtensionContext } from "vscode";
-import { HelloWorldPanel } from "./panels/HelloWorldPanel";
 import { WeatherViewProvider } from "./providers/WeatherViewProvider";
 
 export function activate(context: ExtensionContext) {
-  // Create the show hello world command
-  const showHelloWorldCommand = commands.registerCommand("hello-world.showHelloWorld", () => {
-    HelloWorldPanel.render(context.extensionUri);
-  });
-
-  // Add command to the extension context
-  context.subscriptions.push(showHelloWorldCommand);
-
   // Instantiate a new instance of the WeatherViewProvider class
   const provider = new WeatherViewProvider(context.extensionUri);
 

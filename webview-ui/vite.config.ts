e509424eefc7cdc20125484ef5320ba9a75c7b1a
Ryan Xu <RyanXu1979@gmail.com>
2026-01-14 18:19:44 +0800

    🐳 chore: use react-swc

--- webview-ui/vite.config.ts
+++ webview-ui/vite.config.ts
@@ -1,17 +1,29 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { resolve } from "path"
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "build",
+    sourcemap: "inline",
     rollupOptions: {
       output: {
         entryFileNames: `assets/[name].js`,
         chunkFileNames: `assets/[name].js`,
         assetFileNames: `assets/[name].[ext]`,
       },
+    },
+    chunkSizeWarningLimit: 100000,
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "@components": resolve(__dirname, "./src/components"),
+      "@context": resolve(__dirname, "./src/context"),
+      "@shared": resolve(__dirname, "../src/shared"),
+      "@utils": resolve(__dirname, "./src/utils"),
     },
   },
 });
